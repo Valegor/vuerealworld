63b73daa260c93e76a3e78bd9210ac3d1058de32
Valegor <medifit2000@yandex.ru>
2021-09-09 13:06:47 +0300

    complete authtorization

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
+import {getItem} from '@/helpers/persistanceStorage'
 
 axios.defaults.baseURL = 'https://conduit.productionready.io/api'
+//axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+
+
+
+axios.interceptors.request.use(config => {
+  const token = getItem('accessToken')
+  const authorizisationToken = token ? `Token ${token}` : ''
+  config.headers.Authorization = authorizisationToken
+  return config
+})
 
 export default axios
